30a6cc962cbc9404c95497655bb7ae4979f5d9e0
KeramAyas <ekramayas450@gmail.com>
2026-05-08 00:20:09 -0700

    SCRUM-3  ااااااا

--- Register.jsx
+++ Register.jsx
@@ -9,6 +9,9 @@ function Register({ onLogin }) {
   });
   const [message, setMessage] = useState("");
 #AAAA
+
+
+و
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSubmit = async () => {
