d62a5e308819287529c6d63b130d9621e4c009b3
Tasneem Alrebdawi <tasneemalrebdawi1@gmail.com>
2026-05-08 06:10:36 -0800

    Add files via upload

---
+++ test/success.test.js
@@ -0,0 +1,48 @@
+// src/tests/Login.test.js
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import Login from "../Login";
+
+const mockNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
+
+test("تسجيل الدخول بنجاح", async () => {
+  // Mock الـ fetch
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () =>
+        Promise.resolve({
+          success: true,
+          user: { id: 1, name: "Ahmad" },
+        }),
+    })
+  );
+
+  const onLogin = jest.fn();
+
+  render(
+    <MemoryRouter>
+      <Login onLogin={onLogin} />
+    </MemoryRouter>
+  );
+
+  fireEvent.change(screen.getByRole("textbox"), {
+    target: { name: "email", value: "test@test.com" },
+  });
+
+  fireEvent.change(screen.getByLabelText("كلمة المرور"), {
+    target: { name: "password", value: "123456" },
+  });
+
+  fireEvent.click(screen.getByText("دخول"));
+
+  await waitFor(() => {
+    expect(localStorage.getItem("user")).toContain("Ahmad");
+    expect(onLogin).toHaveBeenCalled();
+    expect(mockNavigate).toHaveBeenCalledWith("/Page");
+  });
+});
