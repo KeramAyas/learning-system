5494549c21bfa8da73e38f6d9dd57a11da00ab0d
KeramAyas <ekramayas450@gmail.com>
2026-05-08 11:57:04 -0700

    Update Register.jsx

--- Register.jsx
+++ Register.jsx
@@ -8,10 +8,7 @@ function Register({ onLogin }) {
     firstName: "", lastName: "", email: "", password: "", confirm: ""
   });
   const [message, setMessage] = useState("");
-#AAAA
 
-
-و
   const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSubmit = async () => {
